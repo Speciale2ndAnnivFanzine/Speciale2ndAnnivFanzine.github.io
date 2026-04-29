a241ccd10b1dcd949b907773c19603e6bc443920
YiQingK <khooyiqing@gmail.com>
2026-04-29 18:12:04 +0800

    image when clicked opens modal

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-/*placeholder*/
+if (!window.modalInitialized) {
+  window.modalInitialized = true
+
+  const imageModal = document.getElementById('imageModal')
+  if (imageModal) {
+    imageModal.addEventListener('show.bs.modal', e => {
+      const { src, caption } = e.relatedTarget.dataset
+      document.getElementById('modalImage').src = src
+      document.getElementById('modalCaption').textContent = caption || ''
+    })
+  }
+}
